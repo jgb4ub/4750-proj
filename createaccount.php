5d19d02e4fda8b8d89e8b102af220aa74b09fc2a
Julia Shea <jts6mq@virginiaedu>
2020-11-12 23:33:54 -0500

    edits to signup and createaccount

--- createaccount.php
+++ createaccount.php
@@ -1,6 +1,6 @@
 <?php
 require 'projectconnect.db';
-
+echo "<p>hi</p>";
 function makeAccount($username, $password, $first_name, $last_name, $email)
 {
     $stmt = $db->prepare('INSERT INTO user (username, password, first_name, last_name, email);
@@ -11,7 +11,7 @@ function makeAccount($username, $password, $first_name, $last_name, $email)
     $stmt->bindParam(':last_name', $last_name, PDO::PARAM_STR);
     $stmt->bindParam(':email', $email, PDO::PARAM_STR);
     $param_password = password_hash($password, PASSWORD_DEFAULT);
-    $stm->execute()
+    $stmt->execute();
 }
 
 function userExists($username)
--- signup.php
+++ signup.php
@@ -4,7 +4,8 @@ require_once "projectconnectdb.php";
 include "createaccount.php";
 
 // Define variables and initialize with empty values
-$username = $password = $first_name = $last_name = $email"";
+$username = $password = $first_name = $last_name = $email = "";
+$username_err = $password_err = 0;
 
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
@@ -14,7 +15,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     if(empty(trim($_POST["password"]))){
         $password_err = "Please enter a password.";
     }
-    else{
+     else{
         $param_username = trim($_POST["username"]);
         $param_password = trim($_POST["password"]);
         $param_first_name = trim($_POST["first_name"]);
@@ -27,34 +28,38 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
  ?>
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Sign Up</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
-    </style>
-</head>
-<body>
-    <div class="wrapper">
-        <h2>Sign Up</h2>
-            <div>
-                <label>Username</label>
-            </div>
-            <div>
-                <label>Password</label>
-            </div>
-            <div>
-                <label>Confirm Password</label>
-            </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Submit">
-            </div>
-            <p>Already have an account?</p>
-        </form>
-    </div>
-</body>
-</html>
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <title>Sign Up</title>
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+     <style type="text/css">
+         body{ font: 14px sans-serif; }
+         .wrapper{ width: 350px; padding: 20px; }
+     </style>
+ </head>
+ <body>
+     <div class="wrapper">
+         <h2>Sign Up</h2>
+         <p>Please fill this form to create an account.</p>
+         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+             <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                 <label>Username</label>
+                 <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+                 <span class="help-block"><?php echo $username_err; ?></span>
+             </div>
+             <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                 <label>Password</label>
+                 <input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
+                 <span class="help-block"><?php echo $password_err; ?></span>
+             </div>
+             <div class="form-group">
+                 <input type="submit" class="btn btn-primary" value="Submit">
+                 <input type="reset" class="btn btn-default" value="Reset">
+             </div>
+             <p>Already have an account? <a href="login.php">Login here</a>.</p>
+         </form>
+     </div>
+ </body>
+ </html>
