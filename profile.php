7810765b99a1b371936857ff59f47023d3738c98
Julia Shea <jts6mq@virginiaedu>
2020-11-15 20:51:36 -0500

    edited profile to include liked reviews and liked restaurants

--- profile.php
+++ profile.php
@@ -2,8 +2,16 @@
 // Include config file
 require("projectconnectdb.php");
 require("profilefunctions.php");
+//session_start();
+//$username = $_SESSION['username'];
 //$restaurantResults = getReviewedRestaurants("byakobowitzl");
 $reviews = getReviews("byakobowitzl");
+$likedreviews = getLikedReviews("byakobowitzl");
+$likedrestaurants = getLikedRestaurants("byakobowitzl");
+//$reviewedrest = getReviewedRestaurants("byakobowitzl");
+$avgRating = getAverageRating("byakobowitzl");
+$username = "byakobowitzl";
+
 $oneStar = " &#9733";
 $twoStars = " &#9733 &#9733";
 $threeStars = " &#9733 &#9733 &#9733";
@@ -16,7 +24,7 @@ $fiveStars = " &#9733 &#9733 &#9733 &#9733 &#9733";
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<title>My Profiles</title>
+<title>My Profile</title>
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,500|Open+Sans">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
@@ -67,6 +75,9 @@ $fiveStars = " &#9733 &#9733 &#9733 &#9733 &#9733";
     padding: 10px;
     margin: 20px
 }
+.text {
+  font-family: Arial, Helvetica, sans-serif;
+}
 
 </style>
 <script>
@@ -84,20 +95,32 @@ $(document).ready(function(){
 });
 </script>
 </head>
-<body>
+    <body>
 <div class="container">
-    <div class="header">
-    <h2 font-family: "Roboto", sans-serif;>My Reviews:</h2>
-    <table class="w3-table w3-bordered w3-card-4 center" style="width:70%">
-      <?php foreach ($reviews as $item): ?>
-      <tr>
-          <div class="row">
-      		<div class="col-lg-8 offset-lg-2">
-      			<div class="accordion mt-5" id="accordionExample">
-      				<div class="card">
-      					<div class="card-header" id="headingOne">
-      						<h2 class="clearfix mb-0">
-      							<a class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne"><?php echo $item['Restaurant_name'];
+    <!-- <div>
+        <h2>My Profile<h2>
+    </div> -->
+    <div>
+        <p class = "text">You are currently logged in as: <?php echo $username ?></p>
+        <p class = "text">Your average rating across all restaurants is: <?php foreach ($avgRating as $item): echo $item['Avg_rating']; endforeach ?></p>
+        <!-- <p class = "text"><?php foreach ($reviewedrest as $item): echo $item['Restaurant_name']; endforeach ?></p> -->
+    </div>
+	<div class="row">
+		<div class="col-lg-8 offset-lg-2">
+			<div class="accordion mt-5" id="accordionExample">
+				<div class="card">
+					<div class="card-header" id="headingTwo">
+						<h2 class="mb-0">
+							<a class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">My Reviews:<i class="fa fa-angle-down"></i></a>
+						</h2>
+					</div>
+					<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+						<div class="card-body" display= "inline-block" vertical-align = "middle">
+                            <table class="w3-table w3-bordered w3-card-4 center" style="width:100%">
+                              <?php foreach ($reviews as $item): ?>
+                              <tr>
+                                <td>
+                                    <h3><?php echo $item['Restaurant_name'];
                                     if ($item['Rating'] == 1){
                                         echo html_entity_decode($oneStar);
                                     }
@@ -112,21 +135,97 @@ $(document).ready(function(){
                                     }
                                     if ($item['Rating'] == 5){
                                         echo html_entity_decode($fiveStars);
-                                    }?><i class="fa fa-angle-down"></i></a>
-      						</h2>
-      					</div>
-      					        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-      						<div class="card-body"><?php echo $item['Review_text']; ?></div>
-      					</div>
-      				</div>
-                </div>
-            </div>
-        </div>
-      </tr>
-      <?php endforeach; ?>
-    </table>
+                                    }
+                                     ?><h3>
+                                    <tr>
+                                        <td>
+                                            <p class="text"> <?php echo $item['Review_text']; ?></p>
+                                        </td>
+                                    </tr>
+                                </td>
+                              </tr>
+                              <?php endforeach; ?>
+                            </table>
+                        </div>
+					</div>
+				</div>
+				<div class="card">
+					<div class="card-header" id="headingThree">
+						<h2 class="mb-0">
+							<a class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">Reviews I've liked: <i class="fa fa-angle-down"></i></a>
+						</h2>
+					</div>
+					<div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+						<div class="card-body">
+                            <table class="w3-table w3-bordered w3-card-4 center" style="width:100%">
+                              <?php foreach ($likedreviews as $item): ?>
+                              <tr>
+                                <td>
+                                    <h3><?php echo $item['Restaurant_name'];
+                                    if ($item['Rating'] == 1){
+                                        echo html_entity_decode($oneStar);
+                                    }
+                                    if ($item['Rating'] == 2){
+                                        echo html_entity_decode($twoStarsStar);
+                                    }
+                                    if ($item['Rating'] == 3){
+                                        echo html_entity_decode($threeStars);
+                                    }
+                                    if ($item['Rating'] == 4){
+                                        echo html_entity_decode($fourStars);
+                                    }
+                                    if ($item['Rating'] == 5){
+                                        echo html_entity_decode($fiveStars);
+                                    }
+                                     ?><h3>
+                                    <tr>
+                                        <td>
+                                            <p class="text"><?php echo $item['Review_text']; ?></p>
 
-</div>
-</div>
+                                        <td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <p class="text">Author: <?php echo $item['Username']; ?></p>
+                                        </td>
+                                    </tr>
+                                </td>
+                              </tr>
+                              <?php endforeach; ?>
+                            </table>
+                        </div>
+					</div>
+				</div>
+                <div class="card">
+					<div class="card-header" id="headingFour">
+						<h2 class="mb-0">
+							<a class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">Restaurants I've Liked: <i class="fa fa-angle-down"></i></a>
+						</h2>
+					</div>
+					<div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
+						<div class="card-body">
+                            <table class="w3-table w3-bordered w3-card-4 center" style="width:100%">
+                              <?php foreach ($likedrestaurants as $item): ?>
+                              <tr>
+                                <td>
+                                    <h3><?php echo $item['Name'];?></h3>
+                                    <tr>
+                                        <td>
+                                            <p class="text"> <?php echo $item['Food_type']; ?></p>
+                                        </td>
+                                    </tr>
+                                </td>
+                              </tr>
+                              <?php endforeach; ?>
+                            </table>
+
+                        </div>
+					</div>
+				</div>
+			</div>
+			</div>
+		</div>
+	</div>
 </body>
+
 </html>
