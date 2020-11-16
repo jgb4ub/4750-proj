1ff75ef0e4297e96d0389eb5776d5e6ea198e621
Lidia Solorzano <les6ye@virginia.edu>
2020-11-15 23:23:37 -0500

    successfully called our stored procedure

--- review_functions.php
+++ review_functions.php
@@ -89,8 +89,7 @@ function addReview( $username, $restaurant_id, $restaurant_name, $review_text, $
 
 
     //Restaurant_rating stored procedure to update
-    $query = $db->prepare("CALL updateRating(?)");
-    $query->bindValue(1,$restaurant_id);
+    $query = $db->prepare("CALL updateRating()");
     if($query->execute()){
         echo "successfully updated ";
     } else {
